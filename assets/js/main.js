a7812c3b0e3248c9b79815456f32e31226df3238
Shaheer Imran <shaheerimran@ShaheersMacBook.T-mobile.com>
2020-09-28 12:02:53 -0400

    Add contact page and clean up code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-// const mobileToggleBtn = document.querySelector('.main-nav__mobile-toggle');
-// const mobileNav = document.querySelector('.mobile-nav');
-
-// mobileToggleBtn.addEventListener('click', () => {
-//     mobileNav.style.visibility = mobileNav.style.visibility === 'visible' ? 'hidden' : 'visible';
-//     mobileToggleBtn.classList.toggle('fa-times');
-// })
-
 const mobileToggleBtn = document.querySelector('.main-nav__mobile-toggle');
 const mobileNav = document.querySelector('.mobile-nav');
 
